00183ac6dff547b22c2d182309fca985f1c4fb1d
Anuj Kumar <160136515+Zero-Onee@users.noreply.github.com>
2024-02-17 14:31:07 +0530

    Update Does array represent Heap.cpp

--- Does array represent Heap.cpp
+++ Does array represent Heap.cpp
@@ -3,10 +3,10 @@ class Solution{
     bool isMaxHeap(int arr[], int n)
     {
         // Your code goes here
-        int i = 0;
+        int i=0;
         while(i < n){
             int x = 2*i+1;
-            if(x > n && arr[i]<arr[x]){
+            if(x < n && arr[i]<arr[x]){
                 return false;
             }
             x = 2*i+2;
