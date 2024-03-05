1abff9392e0579eb0372f7c375b82b3376c6fe42
Anuj Kumar <160136515+Zero-Onee@users.noreply.github.com>
2024-03-05 16:51:03 +0530

    Maximum-Index.cpp
    4.March.2024

---
+++ Maximum-Index.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+        
+    int maxIndexDiff(int a[], int n) 
+    { 
+        map<int,int> mp;
+        int res=0;
+        for(int i=n-1;i>=0;i--){
+            auto it=mp.lower_bound(a[i]);
+            if(it!=mp.end()){
+                res=max(res,it->second-i);
+                mp[a[i]]=it->second;
+            }else{
+                mp[a[i]]=i;
+            }
+        }
+        return res;
+    }
+};
